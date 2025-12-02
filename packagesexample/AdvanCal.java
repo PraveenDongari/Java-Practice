740d60f10c8a85ba58d5f48a7cb7da798437d9e9
PraveenDongari <dongaripraveen30@gmail.com>
2025-12-02 13:31:20 +0530

    Add files via upload

---
+++ packagesexample/AdvanCal.java
@@ -0,0 +1,11 @@
+package packagesexample;
+
+public class AdvanCal {
+    public int multiplication(int i, int j) {
+        return i * j;
+    }
+
+    public int division(int i, int j) {
+        return i / j;
+    }
+}
